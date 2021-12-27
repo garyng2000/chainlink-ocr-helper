ff73c263f755ba20b0ecf72f3bf66892a31275f2
gary <github@garyng.com>
2021-12-27 06:02:01 -0800

    helper dotnet website to test ocr config helper

---
+++ dotnet/website/Test.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Test.aspx.cs" Inherits="Test" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="cTestJSON" TextMode="MultiLine" Width="800" Height="600" runat="server">
+
+            </asp:TextBox>
+            <asp:Button ID="cTestBtn" runat="server" Text="Send" OnClick="TestBtn_Click" />
+            <asp:TextBox ID="cResult" TextMode="MultiLine" Width="800" Height="600" runat="server">
+
+            </asp:TextBox>
+        </div>
+    </form>
+</body>
+</html>
---
+++ dotnet/website/Test.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Chainlink.OCRConfig;
+
+public partial class Test : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            var oracles = ChainlinkOCR.TestOracles();
+            cTestJSON.Text = ChainlinkOCR.ToJson(oracles);
+        }
+    }
+
+    protected void TestBtn_Click(object sender, EventArgs e)
+    {
+        List<OCROracleIdentity> oracles = ChainlinkOCR.FromJson<List<OCROracleIdentity>>(cTestJSON.Text);
+        var s = Enumerable.Range(0, oracles.Count).Select(i => (byte)1);
+        var config = ChainlinkOCR.MakeSetConfigEncodedComponents(NetworkType.Slow, oracles, 10000000 / 100, 1, s.ToArray());
+        var configJson = ChainlinkOCR.ToJson(config);
+
+    }
+}
